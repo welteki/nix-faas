8f0b90c9b40395afcef18e0659ace5514edfb87f
Han Verstraete <welteki@pm.me>
2021-12-02 12:40:47 +0000

    add openfaas tools

--- flake.nix
+++ flake.nix
@@ -13,9 +13,10 @@
     overlay = final: prev: {
       of-watchdog = import ./packages/of-watchdog.nix final;
       classic-watchdog = import ./packages/classic-watchdog.nix final;
+      ofTools = import ./packages/build-support/openfaas final;
     };
 
-  } // utils.lib.eachDefaultSystem (system:
+  } // utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
       pkgs = import nixpkgs {
         inherit system;
@@ -25,6 +26,8 @@
     {
       packages = {
         inherit (pkgs) of-watchdog classic-watchdog;
+        classic-watchdog-image = pkgs.ofTools.baseImages.classic-watchdog;
+        of-watchdog-image = pkgs.ofTools.baseImages.of-watchdog;
       };
 
       devShell = pkgs.mkShell {
---
+++ packages/build-support/openfaas/default.nix
@@ -0,0 +1,84 @@
+{ lib, classic-watchdog, of-watchdog, dockerTools, ... }:
+
+with builtins;
+
+let
+  baseImages =
+    let
+      extraCommands = ''
+        mkdir -p tmp
+        mkdir -p var/openfaas/secrets
+      '';
+    in
+    {
+      classic-watchdog = dockerTools.buildImage {
+        inherit extraCommands;
+
+        name = classic-watchdog.pname;
+        tag = classic-watchdog.version;
+
+        contents = [ classic-watchdog ];
+      };
+
+      of-watchdog = dockerTools.buildImage {
+        inherit extraCommands;
+
+        name = of-watchdog.pname;
+        tag = of-watchdog.version;
+
+        contents = [ of-watchdog ];
+      };
+    };
+
+  buildOrStreamImage =
+    { stream
+    , watchdog ? "classic-watchdog"
+    , watchdogMode ? "streaming"
+    , fprocess ? null
+    , config ? { }
+    , ...
+    }@args:
+
+      assert (!elem watchdog [ "classic-watchdog" "of-watchdog" ]) ->
+        trace "unsupported watchdog `${watchdog}`, must be one of `classic-watchdog` or `of-watchdog`" false;
+      assert (!elem watchdogMode [ "http" "streaming" "serializing" "static" ]) ->
+        trace "unknown watchdogMode `${watchdogMode}`, must be one of `http`, `serializing`, `streaming` or `static`" false;
+
+      let
+        fwatchdog = baseImages.${watchdog};
+
+        buildImage = if stream then dockerTools.streamLayeredImage else dockerTools.buildLayeredImage;
+        acceptedArgs = functionArgs buildImage;
+        args' = intersectAttrs acceptedArgs args;
+      in
+      buildImage (args' // {
+        fromImage = fwatchdog;
+
+        # TODO: better way for merging config?
+        config = config // {
+          Cmd = [ "/bin/fwatchdog" ];
+
+          Env = lib.optionals (config ? Env) config.Env
+            ++ [ "mode=${watchdogMode}" ]
+            ++ lib.optionals (fprocess != null) [ "fprocess=${fprocess}" ];
+
+          ExposedPorts = {
+            "8080" = { };
+          };
+
+          Healthcheck = {
+            Test = [
+              "CMD"
+              "[ -e /tmp/.lock ] || exit 0"
+            ];
+            Interval = 3000000000;
+          };
+        };
+      });
+in
+{
+  inherit baseImages;
+
+  buildOfImage = args: buildOrStreamImage (args // { stream = false; });
+  streamOfImage = args: buildOrStreamImage (args // { stream = true; });
+}
