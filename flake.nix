71cbb601bed90313d7510259c858ae0601b8bb23
Han Verstraete <welteki@pm.me>
2021-12-21 19:59:15 +0000

    add nix-faas package

--- flake.nix
+++ flake.nix
@@ -13,51 +13,77 @@
     extra-trusted-public-keys = [ "welteki.cachix.org-1:zb0txiNEbjq9Fx7svp4LhTgFIQHKSa5ESi7QlLFjjQY=" ];
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs: {
-
-    overlays.default = final: prev: {
-      of-watchdog = import ./pkgs/of-watchdog.nix final;
-      classic-watchdog = import ./pkgs/classic-watchdog.nix final;
-      ofTools = import ./pkgs/build-support/openfaas final;
-
-      hello-fn = import ./examples/hello-fn.nix final;
-    };
-
-  } // utils.lib.eachSystem [ "x86_64-linux" ] (system:
+  outputs = { self, nixpkgs, utils, ... }@inputs:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.default ];
-      };
+      supportedSystems = [ "x86_64-linux" ];
     in
     {
-      packages = {
-        inherit (pkgs) of-watchdog classic-watchdog hello-fn;
-        classic-watchdog-image = pkgs.ofTools.baseImages.classic-watchdog;
-        of-watchdog-image = pkgs.ofTools.baseImages.of-watchdog;
+      overlays.default = final: prev: {
+        of-watchdog = import ./pkgs/of-watchdog.nix final;
+        classic-watchdog = import ./pkgs/classic-watchdog.nix final;
+        ofTools = import ./pkgs/build-support/openfaas final;
+
+        hello-fn = import ./examples/hello-fn.nix final;
+
+        nix-faas =
+          let
+            inherit (final) lib buildGoModule makeWrapper skopeo faas-cli;
+          in
+          buildGoModule {
+            pname = "nix-faas";
+            version = "0.0.1-dev";
+
+            src = ./.;
+            subPackages = [ "cli" ];
+
+            vendorHash = null;
+
+            buildInputs = [ makeWrapper ];
+
+            postInstall = ''
+              makeWrapper $out/bin/cli $out/bin/nix-faas \
+                --prefix PATH : ${lib.makeBinPath [ skopeo faas-cli ]}
+            '';
+          };
       };
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # Golang
-          go
-          gotools
-          gopls
-          go-outline
-          gocode
-          gopkgs
-          gocode-gomod
-          godef
-          golint
-          delve
-          
-          # Nix
-          cachix
-          statix
-          vulnix
-          deadnix
-          nil
-        ];
-      };
-    });
+    } // utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        packages = {
+          inherit (pkgs) nix-faas of-watchdog classic-watchdog hello-fn;
+          classic-watchdog-image = pkgs.ofTools.baseImages.classic-watchdog;
+          of-watchdog-image = pkgs.ofTools.baseImages.of-watchdog;
+
+          default = pkgs.nix-faas;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Golang
+            go
+            gotools
+            gopls
+            go-outline
+            gocode
+            gopkgs
+            gocode-gomod
+            godef
+            golint
+            delve
+            
+            # Nix
+            cachix
+            statix
+            vulnix
+            deadnix
+            nil
+          ];
+        };
+      });
 }
