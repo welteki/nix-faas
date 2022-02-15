89b08f1b096937415610f94edece5dfcbda16421
Han Verstraete <welteki@pm.me>
2022-02-15 15:37:59 +0000

    add description and examples to module options

--- modules/of-stack.nix
+++ modules/of-stack.nix
@@ -29,33 +29,47 @@ in
 {
   options = {
     stackYaml = mkOption {
-      type = package;
       description = "A derivation that produces a stack.yaml file for this function stack.";
+      type = package;
       readOnly = true;
     };
 
     stackYamlText = mkOption {
+      description = "Text of stack.yaml file.";
       type = str;
       readOnly = true;
     };
 
     provider.name = mkOption {
+      description = "The only valid value for provider `name` is `openfaas`.";
       type = str;
       default = "openfaas";
     };
 
     provider.gateway = mkOption {
+      description = "The gateway URL.";
       type = str;
       default = "http://127.0.0.1:8080";
     };
 
     of-stack.extended = mkOption {
+      description = "Attribute set that will be turned into the x-nix-faas section of the stack.yaml file.";
       type = attrs;
     };
 
     functions = mkOption {
-      type = attrsOf (submodule of-function);
       description = "An attribute set of function configurations";
+      type = attrsOf (submodule of-function);
+      default = { };
+      example = {
+        figlet = {
+          image = {
+            name = "docker.io/welteki/figlet";
+          };
+
+          fprocess = ''''${pkgs.figlet}/bin/figlet'';
+        };
+      };
     };
   };
 
